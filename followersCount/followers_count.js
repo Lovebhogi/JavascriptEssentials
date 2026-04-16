e277f910376ccf1af7d882fa8848a4c736e122a8
lovebhogi <bhogendrakusawaha@gmail.com>
2026-04-16 18:02:23 +0545

    if else condition

---
+++ followersCount/followers_count.js
@@ -0,0 +1,26 @@
+let count = 0;
+
+function increaseCount() {
+    count++
+}
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+
+}
+functionincreaseCount() {
+    count++;
+    displayCount();
+}
+
+function checkCountValue() {
+    if (count===10){
+        alert("Your instagram post gained 10 followers");
+    } else if (count===20) {
+        alert("your instagram post gain 20 followers");
+    }
+}
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
