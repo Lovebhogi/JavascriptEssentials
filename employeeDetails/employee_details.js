70692fbde946439d39ea81069b32b604a373609f
lovebhogi <bhogendrakusawaha@gmail.com>
2026-04-17 07:51:08 +0545

    added  Specilization for practice

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,12 +1,12 @@
 const employees = [
-    {id:1,name:'john Doe',age:30,department:'IT',salary:50000},
-    {id:2,name:'Alice Smith',age:28,department:'HR',salary:45000},
-    {id:3,name:'Bob Johnson',age:35,department:'Finance',salary:60000},
+    {id:1,name:'john Doe',age:30,department:'IT',salary:50000,specilization:'Python'},
+    {id:2,name:'Alice Smith',age:28,department:'HR',salary:45000,specilization:'Java'},
+    {id:3,name:'Bob Johnson',age:35,department:'Finance',salary:60000,specilization:'C++'},
 ];
 
 //function to display all the employees
 function displayEmployees(){
-    const totalEmployees=employees.map(employee=>`<p>${employee.id} : ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const totalEmployees=employees.map(employee=>`<p>${employee.id} : ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specilization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML=totalEmployees;
 }
 function calculateTotalSalaries(){
@@ -26,5 +26,13 @@ function findEmployeeById(employeeId){
         document.getElementById('employeesDetails').innerHTML=`<p> ${foundEmployee.id} : ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     } else {
         document.getElementById('employeesDetails').innerHTML=`<p> No Employee Found</p>`;
+    }
+}
+function findEmployeeBySpecilization(employeeSpecilized){
+    const foundEmployee =employees.find(employee=>employee.specilization===employeeSpecilized);
+    if (foundEmployee) {
+         document.getElementById('employeesDetails').innerHTML=`<p> ${foundEmployee.id} : ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specilization}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML=`<p> No Specilization Found</p>`;
     }
 }
